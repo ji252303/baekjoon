791e25d92376edd5cb75d9c365707c5ab7015151
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-04-01 21:12:37 +0900

    깊이우선탐색 내림차순

---
+++ 24480.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+n , m , r = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+cnt = 1
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+for regraph in graph:
+    regraph.sort(reverse=True)
+
+def dfs(start):
+    global cnt
+    visited[start] = cnt
+    for i in graph[start]:
+        if visited[i] == 0:
+            cnt += 1
+            dfs(i)
+
+dfs(r)
+
+for i in range(1, n+1):
+    print(visited[i])
