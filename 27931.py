5afcbe7eacaaa4907a5158cd0e161f36b3e56ade
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-04-17 02:17:48 +0900

    투포인터 풀이추가

--- 27931.py
+++ 27931.py
@@ -64,3 +64,38 @@ if min_odd == float('inf'):
     min_odd = -1 # 홀수 거리의 최솟값이 없을 때 -1 출력
 
 print(min_even, min_odd)
+
+
+
+
+
+다른사람 참고코드 (공부필요)
+
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())  # 점의 개수 n
+dots = list(map(int, input().rstrip().split()))
+dots.sort()
+
+notodd = -1
+odd = -1
+
+for i in range(n - 1) :
+    cnt = 0
+    for j in range(i + 1, n) :
+        d = abs(dots[i] - dots[j])
+
+        if d % 2 == 0 :
+            if notodd == -1 : notodd = d
+            else :
+                if d < notodd : notodd = d
+                else : cnt += 1
+        elif d % 2 == 1 :
+            if odd == -1 : odd = d
+            else :
+                if d < odd : odd = d
+                else : cnt += 1
+        if cnt == 2 : break
+
+print(str(notodd) + " " + str(odd))
