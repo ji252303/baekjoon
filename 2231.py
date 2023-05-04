4683c2b4f34058c9d9f6cafc1095d19049eec41c
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-05-04 21:43:12 +0900

    분해합

---
+++ 2231.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1, n+1):
+    num = sum(map(int, str(i)))
+    num_sum = i+num
+
+    if num_sum == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
