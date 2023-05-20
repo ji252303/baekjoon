be2e96614dd0e0096cc5d5241a2ee3032d4642a3
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-05-20 21:35:23 +0900

    좌표에서 이동시 걸리는 최소시간 출력 (그리디)

---
+++ 1459.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+answer = 0
+
+x, y, w, s = map(int, input().split())
+
+if 2*w <= s:
+    print((x+y)*w)
+else:
+    small = min(x, y)
+    answer = small * s
+    xy = abs(x-y)
+    if w > s:
+        if xy % 2 == 0:
+            answer += xy * s
+        else:
+            answer += (xy-1)*s + w
+    else:
+        answer += xy * w
+    print(answer)
