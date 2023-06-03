608d98284f34357a558baf60b734132d53159f8d
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-06-04 01:57:08 +0900

    자료형에서 문자열,숫자열 판별

---
+++ 1620.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+def input():
+    return stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+by_id = {}
+by_name = {}
+for i in range(1, n + 1):
+    pokemon = input()
+    by_id[i] = pokemon
+    by_name[pokemon] = i
+
+for _ in range(m):
+    x = input()
+    if x.isdigit():
+        print(by_id[int(x)])
+    else:
+        print(by_name[x])
