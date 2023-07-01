e6607280b9f117e98fdbcd5f10e77330f9d413d9
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-07-01 23:26:09 +0900

    이진트리 순회

---
+++ 1991.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+tree = {}
+
+for i in range(n):
+    root, left, right = input().strip().split()
+    tree[root] = [left, right]
+
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
