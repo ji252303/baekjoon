75369b8d25968b4feb273899cd5108bcd2b1ed7a
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-07-06 23:58:55 +0900

    흙길 보수하기 (그리디)
    시간최적화 코드 참고

--- 1911.py
+++ 1911.py
@@ -19,3 +19,23 @@ for start, end in lst:
     last = start
 
 print(count)
+ //위는 시간초과 코드..
+
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(n)]
+
+lst.sort(key=lambda x:x[0])
+last = 0
+res = 0
+
+for start, end in lst:
+    last = max(start, last)
+    diff = end - last
+    count = (diff + l - 1) // l
+    res += count
+    last += count * l
+
+print(res)
