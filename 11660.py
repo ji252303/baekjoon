c8569288466e1993cf64e2b82ca89eacdb32f802
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-07-15 00:16:37 +0900

    Create 11660.py
    https://sodehdt-ldkt.tistory.com/76
    복습하기..

---
+++ 11660.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+dp = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + arr[i-1][j-1]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+
+    answer = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
+    print(answer)
