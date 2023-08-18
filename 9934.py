3db24272dda53aefb97bb341a2b3edf3f7198046
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-08-19 00:41:54 +0900

    Create 9934.py

---
+++ 9934.py
@@ -0,0 +1,18 @@
+def sol(start, end , level):
+    if start == end:
+        ans[level].append(tree[start])
+        return
+    center = (start + end) // 2
+    ans[level].append(tree[center])
+    sol(start, center - 1, level + 1)
+    sol(center + 1, end, level + 1)
+
+
+Level = int(input())
+tree = list(map(int, input().split()))
+l = len(tree)
+ans = [[] for _ in range(Level)]
+
+sol(0, l - 1, 0)
+for a in ans:
+    print(*a)
