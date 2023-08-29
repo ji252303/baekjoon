ee5d2abe2e4c3bf5149c1b249f7635032cd7fe5f
ji252303 <95694431+ji252303@users.noreply.github.com>
2023-08-29 23:17:33 +0900

    연산자 끼워넣기 (백트랙킹)
    순열로 풀면 시간초과 
    나눗셈때 int선언

---
+++ 14888.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))  # +, -, *, //
+
+maximum = -1e9
+minimum = 1e9
+
+def dfs(depth, total, plus, minus, multi, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+
+    if plus:
+        dfs(depth+1, total + num[depth], plus-1, minus, multi, divide)
+    if minus:
+        dfs(depth+1, total - num[depth], plus, minus-1, multi, divide)
+    if multi:
+        dfs(depth+1, total * num[depth], plus, minus, multi - 1, divide)
+    if divide:
+        dfs(depth+1, int(total / num[depth]), plus, minus, multi, divide-1)
+
+
+dfs(1, num[0],op[0],op[1],op[2],op[3])
+print(maximum)
+print(minimum)
